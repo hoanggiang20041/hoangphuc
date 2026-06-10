c870499ecce14732e38c49ef8bbfbafb7a2085b6
hoanggiang20041 <giang10012004@gmail.com>
2026-06-10 21:57:45 +0700

    Fix nodemailer import for ES modules to resolve Vercel 500 error

--- api/contact.js
+++ api/contact.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', true);
